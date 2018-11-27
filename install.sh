0baac75c75bf1a05b96000bc078b802aef197c8b
Donald Cheung <donald@localhost.localdomain>
2018-11-27 13:46:02 +0800

    sh install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 cd ~/Downloads/
 git clone https://github.com/tomasr/molokai.git
 sudo mv molokai/colors/molokai.vim /usr/share/vim/vim81/colors/
+git clone https://github.com/altercation/solarized.git
+sudo mv solarized/vim-colors-solarized/colors/solarized.vim /usr/share/vim/vim81/colors/
 sudo sh -c "cat /home/donald/Downloads/Vim-private/content_added >> /etc/vimrc"
