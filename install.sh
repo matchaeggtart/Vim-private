3c7b6cefe0dc30edee37c65fe51bad79a84ee3d7
matchaeggtart <matchaeggtart@hotmail.com>
2019-02-24 03:32:31 +0800

    sh install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd ~/Downloads/
 #sudo mv molokai.vim /usr/share/vim/vim81/colors/
 #curl -O https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim
 #sudo mv solarized.vim /usr/share/vim/vim81/colors/
-sudo sh -c "cat /home/donald/Downloads/Vim-private/content_added >> /etc/vimrc"
+sudo sh -c "cat /home/jiajun/Downloads/Vim-private/content_added >> /etc/vimrc"
